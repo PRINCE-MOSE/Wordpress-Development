502c2b2899edfc8afea778b8a6e759f928365fb7
PRINCE-MOSE <mosejnr.biz@gmail.com>
2024-02-12 00:08:39 +0300

    Update on Functions and CSS

---
+++ wp-content/themes/wphierarchy/functions.php
@@ -0,0 +1,22 @@
+<?php
+// Adding theme Surport files
+add_theme_support('title_tag');
+add_theme_support('post_thumbnails');
+add_theme_support('post_format',['aside','gallery','link','image','quote','status','video','audio','chat']);
+add_theme_support('html5');
+add_theme_support('automatic-feed-links');
+add_theme_support('custom-background');
+add_theme_support('custom-header');
+add_theme_support('custom-logo');
+add_theme_support('starter-content');
+add_theme_support('customize-selective-refresh-widgets');
+
+// Loading function for CSS
+
+function wphierarchy_enqueue_styles() {
+    wp_enqueue_style('main-css', get_stylesheet_directory_uri().'/style.css', [], time(), 'all' );
+}
+
+add_action('wp_enqueue_scripts','wphierarchy_enqueue_styles');
+
+?>
