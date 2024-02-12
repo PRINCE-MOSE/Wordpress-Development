7876e095eddc2868c139685bc4732f081e52b8e4
PRINCE-MOSE <mosejnr.biz@gmail.com>
2024-02-12 21:24:33 +0300

    Did an update on the footer

--- wp-content/themes/wphierarchy/footer.php
+++ wp-content/themes/wphierarchy/footer.php
@@ -1,8 +1,15 @@
 <?php wp_footer();?>
-<p>This is my Footer </p>
+
 
     </div> <!-- #Content div -->
-</div> <!-- #footer div -->
+</div> <!-- #Page div -->
 
+<footer id="colophon" class="site-footer" role="contentinfo">
+    <a href="<?php echo esc_url(__( 'https://wordpress.org/' , 'wphierarchy')); ?>">
+     <?php printf(esc_html__( 'Prowdly powered by %s','wphierarchy' ), 'wordpress' ); ?>
+    </a>
+</footer>
+
+    <?php wp_footer(); ?>
 </body>
 </html>
