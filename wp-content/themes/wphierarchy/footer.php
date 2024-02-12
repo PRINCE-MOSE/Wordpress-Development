096940e9b7612c09f30f80ec2a87c7ac2eff6235
PRINCE-MOSE <mosejnr.biz@gmail.com>
2024-02-12 16:46:37 +0300

    Added Footer

---
+++ wp-content/themes/wphierarchy/footer.php
@@ -0,0 +1,5 @@
+<?php wp_footer();?>
+<p>This is my Footer </p>
+
+</body>
+</html>
--- wp-content/themes/wphierarchy/index.php
+++ wp-content/themes/wphierarchy/index.php
@@ -1,5 +1,4 @@
 <?php get_header('splash');?>
-    <h1>This is the index</h1>
     
-</body>
-</html>
+    
+<?php get_footer();?>
